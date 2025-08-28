9b9187f35cfd4c3301ced8719b402fa3d15d48dd
zelarin2 <jose.policarpo.oliveira@gmail.com>
2025-08-28 20:26:51 +0100

    style sidebar items

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,23 +9,32 @@ interface Props {
 
 export default function Sidebar({ earthquakes, onClickItem }: Props) {
   return (
-    <aside className='sidebar'>
+    <aside className='sidebar '>
       <h2>Earthquakes</h2>
       <ul>
-        {earthquakes.map(eq => (
-          <li
-            key={eq.id}
-            onClick={() =>
-              onClickItem([
-                eq.geometry.coordinates[1],
-                eq.geometry.coordinates[0],
-              ])
-            }
-            className='my-2 cursor-pointer hover:underline'
-          >
-            <strong>{eq.properties.place}</strong> — Mag {eq.properties.mag}
-          </li>
-        ))}
+        {earthquakes.map(eq => {
+          const date = new Date(eq.properties.time).toLocaleString('en-GB')
+          return (
+            <li
+              key={eq.id}
+              onClick={() =>
+                onClickItem([
+                  eq.geometry.coordinates[1],
+                  eq.geometry.coordinates[0],
+                ])
+              }
+              className='my-4 cursor-pointer group flex flex-row'
+            >
+              <div className='mr-4 self-center min-w-[50px] text-center p-2 bg-red-500 text-white font-bold rounded'>
+                <strong>{eq.properties.mag.toFixed(1)}</strong>
+              </div>
+              <div className='overflow-hidden flex flex-col group-hover:underline'>
+                <strong className='truncate'>{eq.properties.place}</strong>
+                <p>{date}</p>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </aside>
   )
